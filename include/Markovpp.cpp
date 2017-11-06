0107041beb3d2416d4fb5a24713f01b8a04bde7d
Alex Piola <alxpla123@gmail.com>
2017-11-06 19:29:10 +0100

    Add the actual Chain files
    Adds the base Markov chain class

---
+++ include/Markovpp.cpp
@@ -0,0 +1,224 @@
+// BSD 2-Clause License
+// 
+// Copyright (c) 2017, Alex Piola
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "Markovpp.h"
+
+///Get a list of the possible words that can be concatenated
+template <typename T>
+std::vector< T > Chain< T >::GetPossibleWords(const T& word0, const T& word1) const
+{
+    std::vector<T> result;
+    int wordCount = word_list_.size();
+    for( typename std::vector<T>::const_iterator i = word_list_.begin(); i != word_list_.end(); ++i)
+    {
+        T word = *i;
+        int index = i - word_list_.begin();
+        if( word == word0 && index < (wordCount - 1) )
+        {
+            //result.push_back(word_list_[index+1]);
+            if(word_list_[index+1] == word1 && index < wordCount - 2)
+            {
+                result.push_back(word_list_[index+2]);
+            }
+        }
+    }
+    return result;
+}
+
+template <typename T>
+void Chain<T>::AppendToWordList(const std::vector<T>& data)
+{
+    for( typename std::vector<T>::const_iterator i = data.begin(); i != data.end(); ++i )
+        word_list_.push_back( *i );
+}
+
+template <typename T>
+void Chain<T>::AppendToWordList(const T& data)
+{
+    word_list_.push_back( data );
+}
+
+///Generates the phrase/list
+///@param word0 the first "suggestion"
+///@param word1 the second "suggestion"
+///@param maxLen the max lengh of the list
+///@param retries the number of retries if unsuccesfull
+///@param repeatings the number of repeatings/randomizations
+///@param seed the random seed to use
+template<typename T>
+std::vector<T> Chain<T>::GeneratePhrase( const T& word0, const T& word1, const unsigned& maxLen, const int& retries, const int& repeatings, const int& seed )
+{
+    std::vector<T> result;
+    int triesLeft = retries;
+
+    std::unique_ptr<RandInt> rnd( new RandInt( 0, word_list_.size( ) - 1 ) );
+    rnd->Seed( seed );
+
+    std::vector<T> state { word0, word1 };
+
+    result = { state[0], state[1] };
+
+    ///Force max length to be at least 2
+    unsigned mlen = maxLen < 2 ? 2 : maxLen;
+
+    bool done = false;
+    while( !done )
+    {
+        std::vector<T> options = GetPossibleWords( state[0], state[1] );
+        if( options.size( ) > 0 && result.size( ) < mlen )
+        {
+            rnd->SetRange( 0, options.size( ) - 1 );
+            T next = options[rnd->GetRand( )];
+            int repeats = repeatings;
+            while( repeats > 0 )
+            {
+                next = options[rnd->GetRand( )];
+                if( next != state[0] )
+                    repeats = 0;
+                repeats -= 1;
+                state[0] = state[1];
+                state[1] = next;
+                result.push_back( next );
+            }
+        }
+        else
+        {
+            if( triesLeft <= 0 || result.size( ) > mlen )
+                done = true;
+            else
+            {
+                if( result.size( ) < mlen )
+                {
+                    int r0 = rnd->GetRand( );
+                    int r1 = rnd->GetRand( );
+                    state = { word_list_[r0], word_list_[r1] };
+                }
+                triesLeft -= 1;
+            }
+        }
+    }
+    return result;
+}
+
+
+StringChain::StringChain() :
+    Chain<std::string>(),
+    word_string_("")
+{
+    word_list_.clear();
+}
+
+StringChain::~StringChain()
+{
+}
+
+
+std::vector<std::string> StringChain::SplitString(const std::string& text, const char splitchar )
+{
+    std::vector<std::string> result;
+    std::string subword;
+    for(std::string::const_iterator c = text.begin(); c != text.end(); ++c)
+    {
+        if(*c != splitchar)
+            subword.push_back(*c);
+        else
+        {
+            result.push_back(subword);
+            subword = "";
+        }
+    }
+    ///we push back the last word as there's no space to assign.
+    result.push_back(subword);
+    return result;
+}
+
+///Generates a list of words with the given std::string.
+void StringChain::AppendTextToWordList(const std::string& data, const char splitchar)
+{
+    word_string_ = word_string_+data;
+    word_list_.clear();
+    AppendToWordList(SplitString(word_string_, splitchar));
+}
+
+std::string StringChain::GenerateText( const std::string & word0, const std::string & word1, const std::string& separator, const unsigned& maxLen, const int& retries, const int& repeatings, const int& seed )
+{
+    std::unique_ptr<RandInt> rnd( new RandInt( 0, word_list_.size( ) - 1 ) );
+    rnd->Seed( seed );
+    int r0 = rnd->GetRand( );
+    int r1 = rnd->GetRand( );
+
+    std::string word0_ = word_list_[r0];
+    std::string word1_ = word_list_[r1];
+
+    if( word0 != "" )
+        word0_ = word0;
+    if( word1 != "" )
+        word1_ = word1;
+
+    std::vector<std::string> result = GeneratePhrase( word0_, word1_, maxLen, retries, repeatings, seed );
+    std::string resultString = "";
+    for( std::vector<std::string>::iterator i = result.begin(); i != result.end(); ++i)
+    {
+        resultString.append(*i);
+        if(i != (result.end() - 1))
+            resultString.append(separator);
+    }
+    return resultString;
+}
+
+///
+
+IntChain::IntChain() :
+    Chain<int>()
+{
+    word_list_.clear();
+}
+
+IntChain::~IntChain()
+{
+}
+
+void IntChain::AppendIntToWordList(const std::vector<int>& data)
+{
+    AppendToWordList(data);
+}
+
+void IntChain::AppendIntToWordList(const int& data)
+{
+    AppendToWordList(data);
+}
+
+std::vector<int> IntChain::GenerateIntPhraseSingle(const int& word0, const unsigned& maxLen, const int& retries, const int& repeatings, const int& seed)
+{
+    std::vector<int> result;
+    result = GenerateIntPhrase(word0, word0+1, maxLen, retries, repeatings, seed );
+    return result;
+}
+
+std::vector<int> IntChain::GenerateIntPhrase(const int& word0, const int& word1, const unsigned& maxLen, const int& retries, const int& repeatings, const int& seed)
+{
+    return GeneratePhrase(word0, word1, maxLen, retries, repeatings, seed);
+}
---
+++ include/Markovpp.h
@@ -0,0 +1,114 @@
+// BSD 2-Clause License
+// 
+// Copyright (c) 2017, Alex Piola
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <random>
+#include <memory>
+
+class RandInt
+{
+public:
+    RandInt( int lower, int upper ) : seed_(0), lower_(lower), upper_(upper) {}
+
+    int GetRand()
+    {
+        std::uniform_int_distribution<int> dist {lower_,upper_};
+        engine_.seed(seed_);
+        return dist(engine_);
+    }
+
+    void SetRange( int lower, int upper )
+    {
+        lower_ = lower;
+        upper_ = upper;
+    }
+
+    void Seed( int val ) {
+        engine_.seed(val);
+    }
+
+    int operator()() {
+        return GetRand();
+    }
+
+private:
+    int seed_;
+    int lower_;
+    int upper_;
+    std::default_random_engine engine_;
+};
+
+template <typename T>
+class Chain
+{
+public:
+
+    //virtual ~Chain( );
+    std::vector<T> GetWordList() const {
+        return word_list_;
+    }
+
+    int GetWordCount() const {
+        return word_list_.size();
+    }
+
+    std::vector<T> GetPossibleWords( const T& word0, const T& word1 ) const;
+
+protected:
+    ///Do not allow user to create this class.
+    Chain() : word_list_() {};
+    void AppendToWordList( const std::vector<T>& data );
+    void AppendToWordList( const T& data );
+    std::vector<T> word_list_;
+    std::vector<T> GeneratePhrase( const T& word0, const T& word1, const unsigned& maxLen, const int& retries, const int& repeatings, const int& seed );
+};
+
+class StringChain : public Chain<std::string>
+{
+public:
+    StringChain();
+    virtual ~StringChain();
+    static std::vector<std::string> SplitString( const std::string& text, const char splitchar=' ' );
+    void AppendTextToWordList( const std::string& data, const char splitchar=' ' );
+    std::string GenerateText( const std::string& word0, const std::string& word1="", const std::string& separator=" ", const unsigned& maxLen=255, const int& retries=10, const int& repeatings=1, const int& seed=1991 );
+private:
+    std::string word_string_;
+};
+
+class IntChain : public Chain<int>
+{
+public:
+    IntChain();
+    virtual ~IntChain();
+    void AppendIntToWordList(const std::vector<int>& data);
+    void AppendIntToWordList(const int& data);
+    std::vector<int> GenerateIntPhrase( const int& word0, const int& word1, const unsigned& maxLen=255, const int& retries=10, const int& repeatings=1, const int& seed=1991 );
+    std::vector<int> GenerateIntPhraseSingle( const int& word0, const unsigned& maxLen=255, const int& retries=10, const int& repeatings=1, const int& seed=1991 );
+};
+
+
+
+
+
